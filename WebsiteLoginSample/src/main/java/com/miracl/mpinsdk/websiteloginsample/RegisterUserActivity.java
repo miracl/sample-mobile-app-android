7c22fe9e5f17ff1fa1ee5653fcba31d3355f9cae
Neno Stefanov <neno.stefanov@miracl.com>
2019-04-17 16:19:37 +0300

    Change the package name

--- WebsiteLoginSample/src/main/java/com/miracl/mpinsdk/websiteloginsample/RegisterUserActivity.java
+++ WebsiteLoginSample/src/main/java/com/miracl/mpinsdk/websiteloginsample/RegisterUserActivity.java
@@ -1,22 +1,4 @@
-/* **************************************************************
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- ***************************************************************/
-package com.miracl.mpinsdksample;
+package com.miracl.mpinsdk.websiteloginsample;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -33,7 +15,7 @@ import android.widget.Toast;
 import com.miracl.mpinsdk.MPinMfaAsync;
 import com.miracl.mpinsdk.model.Status;
 import com.miracl.mpinsdk.model.User;
-import com.miracl.mpinsdksample.util.ToastUtils;
+import com.miracl.mpinsdk.websiteloginsample.util.ToastUtils;
 
 import static android.view.View.GONE;
 
